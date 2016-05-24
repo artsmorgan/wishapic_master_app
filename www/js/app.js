29f45e2942b44982fe04e344f67966ce50cea9fa
Stephanie <starmed03@gmail.com>
2016-05-24 00:44:42 -0600

    styles and changes on appearance. May 24th.

--- www/js/app.js
+++ www/js/app.js
@@ -22,3 +22,44 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+   //setup state for Home
+  .state('home', {
+      url: '/home',
+      templateUrl: 'templates/home.html'
+  })
+
+ //setup state for login 
+  .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html'
+  })
+
+ //setup state for Get Started 
+  .state('getStarted', {
+      url: '/getStarted',
+      templateUrl: 'templates/getStarted.html'
+  })
+
+  //setup state for Forgot Password
+  .state('forgotPass', {
+      url: '/forgotPass',
+      templateUrl: 'templates/ForgotPass.html'
+  })
+
+  //setup state for Confirm Email
+  .state('confirmEmail', {
+      url: '/confirmEmail',
+      templateUrl: 'templates/confirmEmail.html'
+  })
+
+$urlRouterProvider.otherwise('/home');
+});
